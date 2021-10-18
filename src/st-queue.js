70370d6d9d0e266e6c6467550b8ff5ff525cbab6
Veronika <veron9399bog@gmail.com>
2021-10-19 02:50:41 +0300

    feat: do st-queue

--- src/st-queue.js
+++ src/st-queue.js
@@ -15,7 +15,7 @@ const { ListNode } = require('../extensions/list-node.js');
  */
 module.exports = class Queue {
 	constructor(){
-    this.value=null;
+    this.value = null;
   }
 
   getUnderlyingList() {
@@ -23,11 +23,25 @@ module.exports = class Queue {
   }
 
   enqueue(value) {
- 
+		let current = this.value;
+		
+    while(current && current.next){
+      current = current.next;
+    }
+
+		current === null ? this.value = new ListNode(value) : current.next = new ListNode(value);
   }
 
   dequeue() {
- 
+		let first = this.value,
+    second = first.next;
+
+    let del = this.value.value;
+
+    first.value = second.value;
+    first.next = second.next;
+    
+    return del;
   }
 
 }
